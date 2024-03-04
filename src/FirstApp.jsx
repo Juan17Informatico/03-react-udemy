411d78b622a994e365ccc90d457f2b6a3b452d12
JuanCM <juancampuzano2356@gmail.com>
2024-03-04 13:07:59 -0500

    Colocar estilos de CSS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,13 @@
-const getSaludo = (name) => {
-    return "Hola " + name;
-};
+// const getSaludo = (name) => {
+//     return "Hola " + name;
+// };
 
 export const FirstApp = () => {
     return (
         <>
-            <h1>{getSaludo("Juan")}</h1>
-            {/* <code>{JSON.stringify(newMessage)}</code> */}
+            {/* <h1>{getSaludo("Juan")}</h1>
+            <code>{JSON.stringify(newMessage)}</code> */}
+            <h1>Juan Cam</h1>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {FirstApp} from "./FirstApp";
+import { FirstApp } from "./FirstApp";
 // import { SecondApp } from "./SecondApp";
-
-
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
