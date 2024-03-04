09f73a41b4085317ffe5de2a9308c9f200a077cf
JuanCM <juancampuzano2356@gmail.com>
2024-03-04 10:41:30 -0500

    Nuestro primer componente

---
+++ src/FirstApp.jsx
@@ -0,0 +1,12 @@
+// function App() {
+//     //document.createElement...
+//     return <h1>Hola mundo</h1>;
+// }
+
+// export default App;
+
+export const FirstApp = () => {
+  return (
+    <h1>FirstApp</h1>
+  )
+}
