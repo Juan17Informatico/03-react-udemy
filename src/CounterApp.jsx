fd1e8a33f4afc3e97126c495cc53ae533d2c1f92
JuanCM <juancampuzano2356@gmail.com>
2024-03-06 11:40:24 -0500

    Fin de la sección, creación del counter app

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
+    console.log("render");
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = () => {
+        // console.log('+1');
+        setCounter((c) => c + 1);
+        // setCounter(counter + 1 );
+    };
+
+    const handleSubtract = () => {
+        setCounter((c) => c - 1);
+    };
+
+    const handleReset = () => {
+        setCounter((c) => value);
+    };
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2> {counter} </h2>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubtract}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 };
