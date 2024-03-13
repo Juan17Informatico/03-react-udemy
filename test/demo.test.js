ad03a6b1926fb2ba6e9dd5caa306b4b7d38941cd
JuanCM <juancampuzano2356@gmail.com>
2024-03-13 14:58:24 -0500

    Segundo test a una función que se encuentra dentro de 1 archivo

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,5 +1,12 @@
-test("Esta prueba no debe fallar", () => {
-    if (1 / 0) {
-        throw new Error("No se puede dividir entre cero");
-    }
+describe("Pruebas en <DemoComponent />", () => {
+    test("Esta prueba no debe fallar", () => {
+        // 1. Inicialización
+        const message1 = "HOla mundo";
+
+        // 2. Estimulo
+        const message2 = message1.trim();
+        // 3. Obeservar el comportamiento... esperado
+
+        expect(message2).toBe(message1);
+    });
 });
